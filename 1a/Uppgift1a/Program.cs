7bcd031c14d7018d2765ddf259871a21c74f0af3
jt222ii <jt222ii@student.lnu.se>
2014-11-12 14:21:10 +0100

    Done with tast 1a

---
+++ 1a/Uppgift1a/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userString = Console.ReadLine();
+            int countA = 0;
+            int counta = 0;
+            foreach (char a in userString)
+            {
+                if (a == 'A')
+                { countA++; }
+                if (a == 'a')
+                { counta++; }
+            }
+            Console.WriteLine("antalet stora A var: {0}\nantalet små a var: {1}", countA, counta);
+        }
+    }
+}
