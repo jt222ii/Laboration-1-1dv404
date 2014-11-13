61484b30c07b2f0b5a1694ef63b3049e646e9958
jt222ii <jt222ii@student.lnu.se>
2014-11-13 16:08:16 +0100

    Cleaned up a little

--- Uppgift1c/NastStorsta/Program.cs
+++ Uppgift1c/NastStorsta/Program.cs
@@ -30,13 +30,11 @@ namespace NastStorsta
                     { 
                         highest = userInput;
                         nextHighest = userInput;
-                    //    Console.WriteLine("näst störst: Finns endast ett värde!");
                     }
                     else if (userInput == highest && nextHighest == highest)  // för att man inte ska kunna skriva in 12 rakt igenom och den säger att 12 är näst störst.
                     {                                                         //När nextHighest har fått ett annat värde än highest körs inte längre denna if sats.
                         nextHighest = highest;
                         highest = userInput;
-                        //Console.WriteLine("finns ingen siffra mindre än den största!");
                     }
                     else
                     {
@@ -45,27 +43,11 @@ namespace NastStorsta
                             nextHighest = highest;
                             highest = userInput;
                         }
-                        if(userInput == nextHighest && userInput < highest)
-                        { nextHighest = userInput; }
-                        else if (userInput > nextHighest && userInput < highest && userInput != highest || userInput < nextHighest && nextHighest == highest)
+                        else if (userInput > nextHighest && userInput < highest || userInput < nextHighest && nextHighest == highest)
                         {
                             nextHighest = userInput;
                         }
-                     /*   else if(userInput < nextHighest && nextHighest == highest)
-                        {
-                            nextHighest = userInput;
-                        }*/
-                     //   Console.WriteLine("näst störst: {0}", nextHighest);
-                    }
-                    if(i==amountOfNumbers-1)
-                    {
-                        Console.BackgroundColor = ConsoleColor.DarkGreen;
-                        if(highest == nextHighest)
-                        { Console.WriteLine("finns ingen siffra mindre än den största!"); }
-                        else
-                        Console.WriteLine("näst störst: {0}", nextHighest);
-                        Console.ResetColor();
-                    }
+                    }                  
                     i++;
                 }
                 catch (Exception)
@@ -75,7 +57,14 @@ namespace NastStorsta
                     Console.ResetColor();
                 }
             } while (i < amountOfNumbers);
-            
+
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            if (highest == nextHighest)
+            { Console.WriteLine("finns ingen siffra mindre än den största!"); }
+            else
+                Console.WriteLine("näst störst: {0}", nextHighest);
+            Console.ResetColor();
 
         }
     }
