4fb9a66b156eb7bfa58cbb50a5504a542c55cf7e
jt222ii <jt222ii@student.lnu.se>
2014-11-17 18:00:29 +0100

    Added an alternative solution to 3a

---
+++ Uppgift3a Alternativ Lösning/Palindrom/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine().Replace(" ", "").ToLower();
+            if (userInput.Reverse().SequenceEqual(userInput))  //http://msdn.microsoft.com/en-us/library/bb348567.aspx
+            { Console.WriteLine("det är ett palindrom!"); }
+            else
+            { Console.WriteLine("Inte ett palindrom!"); }
+        }
+    }
+}
