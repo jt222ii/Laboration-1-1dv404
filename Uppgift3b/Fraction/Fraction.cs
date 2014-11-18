ab209785e1eb89aab441116b554957f96812d11b
jt222ii <jt222ii@student.lnu.se>
2014-11-18 17:42:44 +0100

    Started and almost done with 3b

---
+++ Uppgift3b/Fraction/Fraction.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    //        En konstruktor som skapar och initialiserar ett nytt bråktal. 
+    //        Metoderna getNumerator och getDenominator som returnerar täljaren 
+    //      respektive nämnaren. 
+    //      Metoden isNegative som ger true om det är ett negativt bråktal. 
+    //      Metoderna add och multiply som utför motsvarande bråktalsoperation på två 
+    //      bråk och som returnerar ett nytt bråktal. Bestäm själv ett lämpligt sätt att hantera de 
+    //      fall där någon av de inblandade bråktalen har noll i nämnaren. 
+    //      isEqualTo som jämför två Fraction-instanser och ser om de representerar samma 
+    //      bråktal. 
+    //      toString som returnerar en strängrepresentation av bråktalet på form T/
+
+
+    //2.5 timmar 17:30
+    class Fraction
+    {
+        private decimal _numerator;
+        private decimal _denominator;
+        public decimal getNumerator
+        {
+            get { return _numerator; }
+            set { 
+                
+                _numerator = value; }
+        }
+        public decimal getDenominator
+        {
+            get { return _denominator; }
+            set { 
+                if (value == 0)
+                { throw new ArgumentException("Nämnaren kan inte vara noll..."); }
+                _denominator = value; 
+            }
+        }
+        public Fraction(decimal numerator, decimal denominator)
+        {
+            getNumerator = numerator;
+            getDenominator = denominator;
+        }
+        public Fraction add(Fraction a)
+        {
+            decimal newNum = (this.getNumerator * a.getDenominator) + (a.getNumerator * this.getDenominator);
+            decimal newDenom = (this.getDenominator * a.getDenominator);
+            Fraction addResult = new Fraction(newNum, newDenom);
+            return addResult;
+        }
+
+        public Fraction multiply(Fraction a)
+        {
+            decimal newnum = a.getNumerator * this.getNumerator;
+            decimal newdenom = a.getDenominator * this.getDenominator;
+            Fraction multResult = new Fraction(newnum, newdenom);
+            return multResult;
+        }
+
+        public bool isNegative()
+        {
+            //om negativ returnera true
+
+            //annars false
+            return false;
+        }
+
+        public bool isEqualTo(Fraction a)
+        {
+            if (a.getNumerator/a.getDenominator == this.getNumerator/this.getDenominator)
+            {
+                return true;
+            }
+            else 
+            return false;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", getNumerator, getDenominator);
+        }
+    }
+}
---
+++ Uppgift3b/Fraction/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            decimal a;
+            decimal b;
+            decimal c;
+            decimal d;
+            int choice = menu();
+            if(choice == 0)
+            { return; }
+            a = userInput("Ange första bråkets täljare:");
+            b = userInput("Ange första bråkets nämnare:");
+            c = userInput("Ange andra bråkets täljare:");
+            d = userInput("Ange andra bråkets nämnare:");
+            Fraction fraction1 = new Fraction(a, b);
+            Fraction fraction2 = new Fraction(c, d);
+
+            switch(choice)
+            {
+                case 1:
+                    Fraction addResult = fraction1.add(fraction2);
+                    Console.WriteLine(addResult);
+                    break;
+                case 2:
+                    Fraction multResult = fraction1.multiply(fraction2);
+                    Console.WriteLine(multResult);
+                    break;
+                case 3:
+                    bool result = fraction1.isEqualTo(fraction2);
+                    if(result)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("{0} & {1} är lika!", fraction1, fraction2);
+                        Console.ResetColor();
+                        break;
+                    }
+                    else
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} & {1} är INTE lika!", fraction1, fraction2);
+                    Console.ResetColor();
+                    break;
+            }
+
+        }
+        static decimal userInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return decimal.Parse(Console.ReadLine());
+        }
+
+        static int menu()
+        {
+            Console.WriteLine("0. Avsluta 1. Addera Bråktal 2. Multiplicera Bråktal 3. Jämför Bråktal");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
